c9ff13a7843aa2bc5b437ddad90c574f024c77e2
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2022-08-25 16:04:10 +0530

    Add files via upload

---
+++ Reverse_A_Double_LinkedList.java
@@ -0,0 +1,55 @@
+public class Reverse_A_Double_LinkedList {
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+        Node(int x){
+            data=x;
+            prev=null;
+            next=null;
+        }
+    }
+
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }
+        System.out.println();
+    }
+
+    public static Node NodeReverse(Node head){
+        if(head==null || head.next==null)return head;
+        Node prev=null;
+        Node curr=head;
+        while(curr!=null){
+            prev=curr.prev;
+            curr.prev=curr.next;
+            curr.next=prev;
+            curr=curr.prev;
+        }
+        return prev.prev;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+    Node head=new Node(10);
+    Node temp1=new Node(20);
+        Node temp2=new Node(30);
+        head.next=temp1;
+        temp1.prev=head;
+        temp1.next=temp2;
+        temp2.prev=temp1;
+        printlist(head);  // 10 20 30
+        head=NodeReverse(head);
+        printlist(head);  // 30 20 10
+
+    }
+
+}
